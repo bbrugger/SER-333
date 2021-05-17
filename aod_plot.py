53363b433350153fc3b1d044d5897ea4cfad6650
Bruno Brugger <brunobbrugger@gmail.com>
2021-05-17 14:59:12 -0300

    gitignore

---
+++ aod_plot.py
@@ -0,0 +1,10 @@
+import collections
+import csv
+
+aod_data_by_lc = collections.defaultdict(list)
+with open("tab3.csv", encoding="utf-8") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        aod_data_by_lc[row["land_cover"]].append(row)
+
+
