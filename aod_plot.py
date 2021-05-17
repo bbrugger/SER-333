96c286dcb991e326b1ee6ac32a34e79de52fc9d1
Bruno Brugger <brunobbrugger@gmail.com>
2021-05-17 15:03:10 -0300

    numpy and matplotlib

--- aod_plot.py
+++ aod_plot.py
@@ -1,6 +1,9 @@
 import collections
 import csv
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 aod_data_by_lc = collections.defaultdict(list)
 with open("tab3.csv", encoding="utf-8") as csv_file:
     csv_reader = csv.DictReader(csv_file)
